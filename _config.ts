97fbfc6f8fd191aed743de5185959a81361def92
mawathilde <dev@mawathilde.fr>
2023-06-02 22:24:29 +0200

    :sparkles: Basic structure (Style, extentions) (#1)

--- _config.ts
+++ _config.ts
@@ -1,7 +1,13 @@
 import lume from "lume/mod.ts";
+import sass from "lume/plugins/sass.ts";
+import minifyHTML from "lume/plugins/minify_html.ts";
 
 const site = lume({
-    src: "src"
+    src: "src",
+    location: new URL("https://mawathilde.fr")
 });
+
+site.use(sass());
+site.use(minifyHTML());
 
 export default site;
--- src/index.md
+++ src/index.md
@@ -3,4 +3,5 @@ layout: layout.njk
 title: Mawathilde
 ---
 
-# Under construction
+# Under construction 🎉
+Hey ! My website is currently under construction, but you can still check my [Github](https://github.com/mawathilde)
