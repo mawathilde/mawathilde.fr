b3a12b1b03c05561ef58587f451a50e1b9b769b4
mawathilde <dev@mawathilde.fr>
2023-06-02 15:24:50 +0200

    :sparkles: Main.ts (Server)

---
+++ main.ts
@@ -0,0 +1,14 @@
+import Server from "lume/core/server.ts";
+
+const server = new Server({
+	port: 8000,
+	root: `${Deno.cwd()}/_site`
+});
+
+server.use(async (request, next) => {
+	return await next(request);
+});
+
+server.start();
+
+console.log("Listening on http://localhost:8000");
